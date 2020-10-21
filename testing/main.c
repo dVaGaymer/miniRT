913189eeb3fe0e2a8ec3c5c869e7f7055ebf7c9c
Alvaro Lopez <dVaGaymer@gmail.com>
2020-10-21 04:35:53 +0200

    REMOVED TESTING USING GITIGNORE

--- testing/main.c
+++ testing/main.c
@@ -7,33 +7,94 @@ typedef	struct	s_win_info
 	void	*mlx_win;
 }				t_win_info;
 
-void mousePos(int mouse, int x, int y, void *params)
+struct s_canvas_info
 {
-	ft_printf("(%d, %d)\n", x, y);
-	mlx_pixel_put(((t_win_info *)params)->mlx_ptr, ((t_win_info *)params)->mlx_win, x, y, 0x00FF0000);
-}
+	void	*canvas;
+	char	*addr;
+	int		width;
+	int		height;
+	int 	bpp;
+	int		size_line;
+	int		endian;
+};
 
 void close_window(int keycode, void *params)
 {
-	ft_printf("Pressed: %d\n", keycode);
 	if (keycode == 65307)
 	{
 		mlx_destroy_window(((t_win_info *)params)->mlx_ptr, ((t_win_info *)params)->mlx_win);
+		ft_printf("End");
 		exit(0);
 	}
 }
 
+struct s_vec3
+{
+	float x;
+	float y;
+	float z;
+};
+
+struct s_triangle
+{
+	struct s_vec3 p0;
+	struct s_vec3 p1;
+	struct s_vec3 p2;
+};
+
+struct s_ray
+{
+
+};
+
+void fill_pixel(struct s_canvas_info canvas_info, int x, int y, int color)
+{
+	char *dst;
+	dst = canvas_info.addr + canvas_info.size_line * y + x * (canvas_info.bpp / 8);
+	*(unsigned int *)dst = color;
+}
+
+int rgba_to_int(int r, int g, int b, int a)
+{
+	return a << 24 | r << 16 | g << 8 | b;
+}
+
 int     main(void)
 {
-	t_win_info vars;
+	struct s_win_info win_info;
+	struct s_canvas_info canvas_info;
 
-    vars.mlx_ptr = mlx_init();
-    vars.mlx_win = mlx_new_window(vars.mlx_ptr, 800, 600, "Hello world!");
+	//Setup
+	canvas_info.width = 800;
+	canvas_info.height = 600;
+    win_info.mlx_ptr = mlx_init();
+    win_info.mlx_win = mlx_new_window(win_info.mlx_ptr, canvas_info.width, canvas_info.height, "Hello world!");
 
-	ft_printf("Hola");
 
-	mlx_mouse_hook(vars.mlx_win, mousePos, (void *)&vars);
-	mlx_key_hook(vars.mlx_win, close_window, (void *)&vars);
+	canvas_info.canvas = mlx_new_image(win_info.mlx_ptr, canvas_info.width, canvas_info.height);
+	canvas_info.addr = mlx_get_data_addr(canvas_info.canvas, &canvas_info.bpp, &canvas_info.size_line, &canvas_info.endian);
 
-    mlx_loop(vars.mlx_ptr);
+	//Hook events
+	mlx_key_hook(win_info.mlx_win, close_window, (void *)&win_info);
+
+	//Draw on canvas
+	int color, r, g, b;
+	for (int y = 0; y < canvas_info.height; y++)
+	{
+		for (int x = 0; x < canvas_info.width; x++)
+		{
+			r = (255 * (1 - (float)y / (float)canvas_info.height));
+			g = (255 * ((float)y / (float)canvas_info.height));
+			b =	(255 * ((float)x / (float)canvas_info.width));
+			
+			color = rgba_to_int(r, g, b, 0);
+			fill_pixel(canvas_info, x, y, mlx_get_color_value(win_info.mlx_ptr, color));
+		}
+	}
+	ft_printf("Img filled!\n");
+
+	//Add canvas to window
+	mlx_put_image_to_window(win_info.mlx_ptr, win_info.mlx_win, canvas_info.canvas, 0, 0);
+
+    mlx_loop(win_info.mlx_ptr);
 }    
