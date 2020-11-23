3509f36f24c02194a3be07e075db769089442782
Alvaro Lopez <dVaGaymer@gmail.com>
2020-11-23 03:04:17 +0100

    Definitely added separated folder "Testing" to work with ideas :P

---
+++ testing/config.sh
@@ -0,0 +1,17 @@
+git submodule init
+git submodule update
+
+MINIRT_DIR=../
+export MINIRT_DIR
+
+#Add absolute path here
+LIBFT_DIR=/home/alopez-g/Documents/miniRT/srcs/libs/Libft/
+export LIBFT_DIR
+
+#Add absolute path here
+FTPF_DIR=/home/alopez-g/Documents/miniRT/srcs/libs/FTPF/
+export FTPF_DIR
+
+#Add absolute path here
+GNL_DIR=/home/alopez-g/Documents/miniRT/srcs/libs/GNL/
+export GNL_DIR
